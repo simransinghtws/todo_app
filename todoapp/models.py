04103af6c33fbcc4e6a709718d3ab2cf0e1df3b4
sarbjitgrewal <sarbjitgrewaltekkiweb@gmail.com>
2023-09-27 15:16:48 +0530

    update models

--- todoapp/models.py
+++ todoapp/models.py
@@ -10,9 +10,9 @@ class Todo(models.Model):
         return self.title
     
 class TodoList(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    todo=models.ForeignKey(Todo,on_delete=models.CASCADE)
-    def __str__(self):
-        return self.user.username
+    # user=models.ForeignKey(User,on_delete=models.CASCADE)
+    # todo=models.ForeignKey(Todo,on_delete=models.CASCADE)
+    # def __str__(self):
+    #     return self.user.username
     
   
