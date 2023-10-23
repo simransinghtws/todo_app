a7c9bb33f8289e4424ee99a54402324cedc04423
dalbeertws <dalbeer.tws@tekkiwebsolutions.com>
2023-10-23 11:47:49 +0530

    test

--- Todo/settings.py
+++ Todo/settings.py
@@ -121,7 +121,7 @@ USE_I18N = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/xx'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
